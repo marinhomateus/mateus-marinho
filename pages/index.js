dcb126bc2cf4817e814e58e1950a6dcb08e646fc
Mateus Marinho <mateus.marinho@hotmail.com.br>
2021-07-26 02:20:41 -0300

    navbar

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import NavBar from './../src/components/NavBar'
 
 const Title = styled.h1`
   font-size: 50px;
@@ -6,5 +7,9 @@ const Title = styled.h1`
 `
 
 export default function Home() {
-  return <Title>My portfolio</Title>
+  return (
+  <>
+    <NavBar/>
+  </>
+  );
 }
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NavBar = () => {
+  return ({})
+}
+export default NavBar;
