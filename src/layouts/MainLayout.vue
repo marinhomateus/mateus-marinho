c72c1ec5f8126aa4efa7dc36b3122d73b60100a1
Mateus Marinho <mateus.marinho@hotmail.com.br>
2021-12-02 17:28:31 -0300

    clear

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,39 +2,27 @@
   <v-app>
     <v-app-bar flat height="50" app color="white">
       <v-layout align-center>
+        <v-icon src="@assets/logo.png"> </v-icon>
         <v-flex>
-          <v-text-field
-            color="grey"
-            placeholder="Search"
-            class="mt-7"
-            flat
-            solo-inverted
-            prepend-inner-icon="mdi-magnify"
-          />
+          Mateus Marinho
         </v-flex>
+        <v-menu v-if="more.length" bottom left>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn text class="align-self-center mr-4" v-bind="attrs" v-on="on">
+              <v-icon right>
+                mdi-menu-down
+              </v-icon>
+            </v-btn>
+          </template>
 
-        <v-btn height="45" class="mx-2 ml-4">
-          <v-icon> mdi-compare-vertical </v-icon>
-        </v-btn>
-        <v-btn height="45" class="ml-2" dark>
-          <v-icon> mdi-plus </v-icon>
-        </v-btn>
+          <v-list class="grey lighten-3">
+            <v-list-item v-for="item in more" :key="item" to="#">
+              {{ item }}
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </v-layout>
     </v-app-bar>
-
-    <v-navigation-drawer app expand-on-hover right>
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
 
     <v-main>
       <home-page />
@@ -50,15 +38,7 @@ export default {
   },
   data() {
     return {
-      items: [
-        { title: "Home", icon: "mdi-home" },
-        { title: "Recent", icon: "mdi-clock-time-three" },
-        { title: "Following", icon: "mdi-account-group" },
-      ],
-      items2: [
-        { title: "Messages", icon: "mdi-view-dashboard" },
-        { title: "Notifications", icon: "mdi-image" },
-      ],
+      more: ["About", "Projects", "Contact"],
     };
   },
 };
